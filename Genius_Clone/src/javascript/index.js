f56152351b50f55a308e56c8114266adf0ec13a7
gut0Rodri <carlos.augusto01503@hotmail.com>
2026-08-15 19:00:43 -0300

    Update index.js

--- Genius_Clone/src/javascript/index.js
+++ Genius_Clone/src/javascript/index.js
@@ -1,28 +1,77 @@
-const buttonsTableGame = document.querySelectorAll(".table-game button");
+const buttonInitGame = document.getElementById('button-init-game')
+const currentScoreHTML = document.getElementById('current-score')
+const previuosScoreHTML = document.getElementById('previous-score')
+const bestScoreHTML = document.getElementById('best-score')
 
-const sequence = ["green", "yellow", "red"];
-const sequenceUser = [];
+let currentScore = 0;
+let previuosScore = 0;
+let bestScore = 0;
 
-buttonsTableGame.forEach((item) => {
-  item.addEventListener("click", (event) => {
-    let color = event.target.getAttribute("id");
+currentScoreHTML.textContent = currentScore
+previuosScoreHTML.textContent = previuosScore
+bestScoreHTML.textContent = bestScore
 
-    sequenceUser.push(color);
+// LISTA DE CORES DISPONÍVEIS
+const colors = ["green", "red", "blue", "yellow"];
 
-    checkSequence()
+// LISTA ATUAL DE CORES ALEATÓRIAS
+const currentSequence = [];
 
-  });
-});
+// LISTA DO USUÁRIO
+const userSequence = [];
 
-function checkSequence() {
-  sequenceUser.forEach((item, index) => {
-    if (item !== sequence[index]) {
-      console.log("Errado");
-      sequenceUser.splice(0, sequenceUser.length);
+// FUNÇÃO DE CONTAGEM REGRESSIVA PARA INICIAR O JOGO
+function countdown () {
+  // TEMPO PARA CONTAGEM REGRESSIVA
+  let time = 3;
+
+  // CONTAINER ONDE IRÁ APARECER A CONTAGEM
+  let container = document.querySelector('.circle-mid');
+
+  // DESABILITANDO O BOTÃO
+  buttonInitGame.disabled = true
+  buttonInitGame.classList.remove('usable')
+
+  // LÓGICA PARA CONTAGEM REGRESSIVA
+  const interval = setInterval(() => {
+    if (time > 0) {
+      container.textContent = time
+      time--;
+    } else {
+      container.textContent = 'Go'
+
+      const timeout = setTimeout(() => {
+        container.textContent = ''
+
+        randomColor();
+
+        clearInterval(interval)
+        clearTimeout(timeout)
+      }, 1000)
       return;
     }
-
-    console.log(item, index, 'Correto');
-    return;
-  });
+  }, 1000)
 }
+
+function randomColor () {
+  const randomIndex = Math.floor(Math.random() * colors.length)
+  currentSequence.push(colors[randomIndex])
+  return;
+}
+
+function activeColor () {
+  currentSequence.forEach((item, index) => {
+    const buttonColor = document.querySelector(`#${item}`);
+
+    setTimeout(() => {
+      buttonColor.classList.add("active")
+
+      setTimeout(() => {
+        buttonColor.classList.remove("active")
+      }, 500)
+    }, 0 + (1000 * index))
+  })
+
+  return;
+}
+
